7f282c480bcaacec4e02acf257b71af92e0ab362
Emilia Alves <enbrakaffe@gmail.com>
2024-09-13 16:45:55 -0300

    feat: Database and Dotenv are now properly configured

---
+++ src/test/java/dev/tarffie/TreeCitySessions/TreeCitySessionsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tarffie.TreeCitySessions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreeCitySessionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
